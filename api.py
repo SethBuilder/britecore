4a7e84637d452cb015504c37977a568298a762c3
Seif <moghrabi@gmail.com>
2018-02-14 17:19:01 +0200

    enum solved - data is returned from api for getInsurancePolicyForm()

--- api.py
+++ api.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request, render_template
-from app import ceateTables
+from app import ceateTables, getTables
 import logging
+from sqlalchemy import MetaData
 logging.basicConfig(filename='example.log', filemode='w', level=logging.DEBUG)
 
 class CustomFlask(Flask):
+	metadata = MetaData()
 	jinja_options = Flask.jinja_options.copy()
 	jinja_options.update(dict(
 	block_start_string='(%',
@@ -24,7 +26,12 @@ def index(name=None):
 @app.route('/risks', methods=['POST'])
 def postInsurancePolicySpecs():
 	logging.info('This goes to log file: '+str(request.json))
-	return(ceateTables(request.json))
+	return(ceateTables(request.json, app.metadata))
+
+@app.route('/risks', methods=['GET'])
+def getInsurancePolicyForm():
+	logging.info('This goes to log file: '+str(request.json))
+	return(getTables(app.metadata))
 
 if __name__ == '__main__:':
 	app.run(debug=True)
--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
-from sqlalchemy import MetaData
+from flask import Flask, jsonify
+
 from sqlalchemy import Table, Column
-from sqlalchemy import Integer, String, DateTime, Numeric, Enum
+from sqlalchemy import Integer, String, DateTime, Numeric
+from sqlalchemy.dialects.mysql import ENUM
+# from enum import Enum
+# from sqlalchemy.types import Enum
 from sqlalchemy import create_engine
 import os
+import ast
 
 class CustomFlask(Flask):
-	""" This class is so that I can work with Flask and Vue templates together | P.S I didn't create this"""
+	""" This class is so that I can work with Flask and Vue templates together"""
+	
 	jinja_options = Flask.jinja_options.copy()
 	jinja_options.update(dict(
 	block_start_string='(%',
@@ -18,30 +23,21 @@ class CustomFlask(Flask):
 	))
 
 
+
+
+
+
 # Create Flask app
 app = CustomFlask(__name__)
 
-def ceateTables(risks):
+def ceateTables(risks, metadata):
 	"""Takes a list of objects representing risks and fields for each risk,
 	Then it creates a representation for these tables and creates them afterwards | all the tables have a MetaData() object as foreign key,
 	So that each set of tables are seperate and possibly assigned to a specific user"""
 
-	# To create a clean database for each form submit
-	try:
-		os.remove('test.db')
-	except OSError:
-		pass
-	
-
 	# Connect to database
 	# engine = create_engine("sqlite:///test.db", echo=True)
 	engine = create_engine('mysql+mysqldb://root:123456@localhost/britecore')
-
-	# Create a metadata object for posted risks.
-	# MetaData() is a collection that we can add set of tables to and traverse them like an XML DOM
-	metadata = MetaData()
-
-	
 
 	print("POSTED DATA: "+str(risks))
 
@@ -53,13 +49,44 @@ def ceateTables(risks):
 	metadata.create_all(engine)
 	return "Hello"
 
+def getTables(metadata):
+	print("BEHOLD TABLE NAMES: "+str(metadata.sorted_tables))
+	# [
+	#{'table_name': 'Jim's car', 'fields': [{'field': 'Price', 'type': 'String'}]}
+	#]
+	tables = metadata.sorted_tables
+	table_names = [table.name for table in tables]
+	field_types = []
+	for table in tables:
+		field_names = [column.name for column in table.columns]
+		field_types_initial = [column.type for column in table.columns]
+		for field_type in field_types_initial:
+			if isinstance(field_type, ENUM):
+				field_type = ast.literal_eval(str(field_type)[4:].replace('(', '[').replace(')', ']'))
+				
+				field_types.append(field_type)
+			else:
+				field_types.append(field_type)
+		
+	# fields = [(field_name, data_type) for field_name, data_type in zip(field_names, field_types) ]
+	print("XXX"+str(field_types))
+	# fields = [{k:v for k,v in fields}]
+	
+	print(str(table_names))
+
+	tables_and_their_fields = []
+
+	return 'Hi'
+
+def getFieldNamesFromDB(metadata):
+	pass
+
 
 def ceateTableRepresentation(risk, metadata):
-	table_name = risk['risk_name'].replace(' ', '-').lower()
+	table_name = risk['risk_name'].replace(' ', '-').lower()+"-table"
 	table_display_name = risk['risk_name'].capitalize()
 	field_names = getFieldNames(risk['fields'])
 	data_types = getDataTypes(risk['fields'])
-	print(field_names)
 
 	table = Table(table_name, metadata,
 		Column('id', Integer, primary_key=True),
@@ -73,32 +100,23 @@ def getFieldNames(fields):
 	return [field['field'] for field in fields]
 
 def getDataTypes(fields):
-	types = []
+	data_types = []
 	for field in fields:
-		type_list = field['type']
-		if len(type_list) == 1:
-			if type_list[0] == 'text':
-				types.append(String(50))
-			elif type_list[0] == 'number':
-				types.append(Numeric())
-			elif type_list[0] == 'date':
-				types.append(DateTime)
-
-		elif len(type_list) == 2:
-			print("ccccccc"+type_list)
-			if all(x in types for x in ['text','number']):
-				types.append(Enum('String(50)', 'Numeric()'))
-			elif all(x in types for x in ['text', 'date']):
-				types.append(Enum('String(50)', 'DateTime'))
+		types_list = field['type']
+		print("FIELD types: " + str(types_list))
+		if isinstance(types_list, str):
+			if types_list == 'text':
+				data_types.append(String(50))
+			elif types_list == 'number':
+				data_types.append(Numeric())
+			elif types_list == 'date':
+				data_types.append(DateTime)
 			else:
-				types.append(Enum('DateTime', 'Numeric()'))
-
+				data_types.append(ENUM(types_list[0]))
 
 		else:
-			types.append(Enum('String(50)', 'DateTime', 'Numeric()'))
-
-
-	return types
+			data_types.append(ENUM(*(types_list)))
+	return data_types
 
 if __name__ == '__main__:':
 	app.run(debug=True)
